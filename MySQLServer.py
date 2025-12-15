3027e9325996edc2d6d92475524214dc68451fd2
Onyinye Ovuh <onyinyeovuh@gmail.com>
2025-12-15 22:21:57 +0100

    Add Script to create alx_books_store database

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    # CONNECT TO MYSQL SERVER
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="08065618424@Oop"
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        # CREATE DATABASE (DOES NOT FAIL IF IT EXISTS)
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print("Error while connecting to MySQL")
+    print(e)
+
+finally:
+    # CLOSE CONNECTION
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
